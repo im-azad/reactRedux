cc7e42c87e647b4cc5863596f9690cb6457d32be
im-azad <me.azad99@gmail.com>
2023-10-25 23:27:14 +0600

    cunter set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Counter from "./components/Counter";
 
-function App() {
+export default function App() {
+    return (
+        <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+            <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
+                Simple Counter Application
+            </h1>
 
-  return (
-    <>
-   <h1>Hello World</h1>
-
-    </>
-  )
+            <div className="max-w-md mx-auto mt-10 space-y-5">
+                <Counter />
+                <Counter />
+                <Counter />
+            </div>
+        </div>
+    );
 }
-
-export default App
