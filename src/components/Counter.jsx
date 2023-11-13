5ca3afe9175d0405177b210a0742bc644f5e394d
im-azad <me.azad99@gmail.com>
2023-11-13 22:29:46 +0600

    useDelector and useDispatch redux hook use

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,22 +1,30 @@
-import { connect } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { decrement, increment } from "../redux/counter/actions";
 
 // eslint-disable-next-line react/prop-types
-function Counter({ count, increment, decrement, id }) {
+function Counter() {
+  const count = useSelector((state) => state.value);
+  const dispatch = useDispatch();
+
+  const incrementHandler = () => {
+    dispatch(increment());
+  };
+  const decrementHandler = () => {
+    dispatch(decrement());
+  };
   return (
     <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
-      <p>Counter ID: {id}</p>
       <div className="text-2xl font-semibold">{count}</div>
       <div className="flex space-x-3">
         <button
           className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
-          onClick={increment}
+          onClick={incrementHandler}
         >
           Increment
         </button>
         <button
           className="bg-red-400 text-white px-3 py-2 rounded shadow"
-          onClick={decrement}
+          onClick={decrementHandler}
         >
           Decrement
         </button>
@@ -25,21 +33,4 @@ function Counter({ count, increment, decrement, id }) {
   );
 }
 
-const mapStateToProps = (state, ownProps) => {
-  console.log("ownProps:", ownProps);
-  return {
-    count: state.value,
-  };
-};
-const mapDispatchToProps = (dispatch) => {
-  return {
-    increment: (value) => dispatch(increment(value)),
-    decrement: (value) => dispatch(decrement(value)),
-  };
-};
-
-// higher order function that take a component as a parameter and return new component
-// const NewComponent = HOC(OriginalComponent)
-// connect() করলে একটা HOC পাই (component_pass)
-
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+export default Counter;
