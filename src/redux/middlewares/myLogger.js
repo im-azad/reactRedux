1740a271cd4f755e33187cfa363c7fd52b73dcde
im-azad <me.azad99@gmail.com>
2023-11-14 21:52:25 +0600

    multiple middleware use

---
+++ src/redux/middlewares/myLogger.js
@@ -0,0 +1,14 @@
+import rootReducer from "../rootReducer";
+
+//function from callable as f(a, b, c) into callable as f(a)(b)(c).
+const myLogger = (store) => (next) => (action) => {
+  console.log(`Action: ${JSON.stringify(action)}`);
+  console.log(`Before: ${JSON.stringify(store.getState())}`);
+  const upcomingState = [action].reduce(rootReducer, store.getState()); // array reduce a single value
+
+  console.log(`Upcoming State: ${JSON.stringify(upcomingState)}`);
+
+  return next(action);
+};
+
+export default myLogger;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,8 @@
 import { applyMiddleware, createStore } from "redux";
 
 import rootReducer from "./rootReducer";
+import myLogger from "./middlewares/myLogger";
+import logger from "redux-logger";
 
-//function from callable as f(a, b, c) into callable as f(a)(b)(c).
-const myLogger = (store) => (next) => (action) => {
-  console.log(`Action: ${JSON.stringify(action)}`);
-  console.log(`Before: ${JSON.stringify(store.getState())}`);
-  const upcomingState = [action].reduce(rootReducer, store.getState()); // array reduce a single value
-
-  console.log(`Upcoming State: ${JSON.stringify(upcomingState)}`);
-
-  return next(action);
-};
-
-const store = createStore(rootReducer, applyMiddleware(myLogger));
+const store = createStore(rootReducer, applyMiddleware(logger, myLogger));
 export default store;
