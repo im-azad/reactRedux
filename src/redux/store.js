47d498e22a6e92857b875cdde4dfbfe71abdb6ee
im-azad <me.azad99@gmail.com>
2023-11-14 21:42:50 +0600

    applyMiddleware use a coustom logger create

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,17 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 
 import rootReducer from "./rootReducer";
 
-const store = createStore(rootReducer);
+//function from callable as f(a, b, c) into callable as f(a)(b)(c).
+const myLogger = (store) => (next) => (action) => {
+  console.log(`Action: ${JSON.stringify(action)}`);
+  console.log(`Before: ${JSON.stringify(store.getState())}`);
+  const upcomingState = [action].reduce(rootReducer, store.getState()); // array reduce a single value
+
+  console.log(`Upcoming State: ${JSON.stringify(upcomingState)}`);
+
+  return next(action);
+};
+
+const store = createStore(rootReducer, applyMiddleware(myLogger));
 export default store;
